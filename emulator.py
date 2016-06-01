eeab787d3483fea79c203ca4f5db6641958872b4
JaeHyuk Lim <JaeHyuk Lim>
2016-06-01 05:04:09 -0700

    Change

---
+++ emulator.py
@@ -0,0 +1,12 @@
+import vcpu_define
+from convert_reg import *
+from struct import *
+
+a1_word, a2_word, a1_dword, a2_dword = 0, 0, 0, 0;
+a1_byte, a2_byte = 0, 0;
+class AsmEmulator:
+	def BranchType(self, code):
+		
+	def Single_OpCode(self, data):
+
+	def Double_OpCode(self, data):
